90c82964a64acc85f9a09deca53bc4eb76f8ef52
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2023-07-04 22:34:08 +0200

    Nested terms in index

--- elastic_index.py
+++ elastic_index.py
@@ -7,8 +7,8 @@ import math
 class Index:
     def __init__(self, config):
         self.config = config
-        # self.es = Elasticsearch(hosts=[{"host": "ecodices_es"}], retry_on_timeout=True)
-        self.client = Elasticsearch()
+        self.client = Elasticsearch(hosts=[{"host": "esta_es"}], retry_on_timeout=True)
+        # self.client = Elasticsearch()
 
     def no_case(self, str_in):
         str = str_in.strip()
@@ -48,24 +48,25 @@ class Index:
         response = self.client.search(
             index="esta",
             body={
-            "size": 0,
-            "aggs": {
-                "nested_terms": {
-                    "nested": {
-                        "path": "sub_voyage"
-                    },
-                    "aggs": {
-                        "filter": {
+                "size": 0,
+                "aggs": {
+                    "nested_terms": {
+                        "nested": {
+                            "path": "sub_voyage"
+                        },
+                        "aggs": {
                             "filter": {
-                                "regexp": {
-                                    field: facet_filter + ".*"
-                                }
-                            },
-                            "aggs": {
-                                "names": {
-                                    "terms": {
-                                        "field": field + ".keyword",
-                                        "size": amount
+                                "filter": {
+                                    "regexp": {
+                                        field: facet_filter + ".*"
+                                    }
+                                },
+                                "aggs": {
+                                    "names": {
+                                        "terms": {
+                                            "field": field + ".keyword",
+                                            "size": amount
+                                        }
                                     }
                                 }
                             }
@@ -73,27 +74,25 @@ class Index:
                     }
                 }
             }
-        }
-
         )
-
         for hits in response["aggregations"]["nested_terms"]["filter"]["names"]["buckets"]:
             buffer = {"key": hits["key"], "doc_count": hits["doc_count"]}
             ret_array.append(buffer)
         return ret_array
 
-
     def browse(self, page, length, orderFieldName, searchvalues):
         int_page = int(page)
         start = (int_page - 1) * length
-        matches = []
+        must_collection = []
 
         if searchvalues == []:
             response = self.client.search(
                 index="esta",
                 body={"query": {
                     "match_all": {}},
-                    "_source": ["year", "summary", "voyage_id", "sub_voyage.sub_dept_location", "sub_voyage.dep_latitude", "sub_voyage.dep_logitude", "sub_voyage.arrival_latitude", "sub_voyage.arrival_longitude", "sub_voyage.sub_arrival_location"],
+                    "_source": ["year", "summary", "voyage_id", "sub_voyage.sub_dept_location",
+                                "sub_voyage.dep_latitude", "sub_voyage.dep_logitude", "sub_voyage.arrival_latitude",
+                                "sub_voyage.arrival_longitude", "sub_voyage.sub_arrival_location"],
                     "size": length,
                     "from": start
                 }
@@ -102,31 +101,57 @@ class Index:
             for item in searchvalues:
                 if item["field"] == "FREE_TEXT":
                     for value in item["values"]:
-                        matches.append({"multi_match": {"query": value, "fields": ["*"]}})
+                        must_collection.append({"multi_match": {"query": value, "fields": ["*"]}})
+                elif item["field"] == "year":
+                    range_values = item["values"][0]
+                    r_array = range_values.split('-')
+                    must_collection.append({"range": {item["field"]: {"gte": r_array[0], "lte": r_array[1]}}})
+                elif item["field"] == "sub_voyage.slaves_total":
+                    range_values = item["values"][0]
+                    r_array = range_values.split('-')
+                    must_collection.append({
+                        "nested": {
+                            "ignore_unmapped": "true",
+                            "path": "sub_voyage",
+                            "query": {
+                                "range": {
+                                    "sub_voyage.slaves_total": {
+                                        "gte": r_array[0],
+                                        "lte": r_array[1]
+                                    }
+                                }
+                            }
+                        }
+                    })
                 else:
-                    if item["field"] in self.config["ranges"]:
-                        range_values = item["values"][0]
-                        r_array = range_values.split('-')
-                        matches.append({"range": {item["field"]: {"gte": r_array[0], "lte": r_array[1]}}})
-                    else:
-                        if len(item["values"]) > 1:
-                            matches.append({"terms": {item["field"] + ".keyword": item["values"]}})
-                        else:
-                            matches.append({"term": {item["field"] + ".keyword": item["values"][0]}})
+                    must_collection.append(
+                        {
+                            "nested": {
+                                "ignore_unmapped": "true",
+                                "path": "sub_voyage",
+                                "query": {
+                                    "terms": {
+                                        item["field"] + ".keyword": item["values"]
+                                    }
+                                }
+                            }
+                        }
+                    )
+
 
             response = self.client.search(
                 index="esta",
                 body={"query": {
                     "bool": {
-                        "filter": matches
+                        "must": must_collection
                     }},
-                    "_source": ["year", "summary", "voyage_id", "sub_voyage.sub_dept_location", "sub_voyage.dep_latitude", "sub_voyage.dep_logitude", "sub_voyage.arrival_latitude", "sub_voyage.arrival_longitude", "sub_voyage.sub_arrival_location"],
+                    "_source": ["year", "summary", "voyage_id", "sub_voyage.sub_dept_location",
+                                "sub_voyage.dep_latitude", "sub_voyage.dep_logitude", "sub_voyage.arrival_latitude",
+                                "sub_voyage.arrival_longitude", "sub_voyage.sub_arrival_location"],
                     "size": length,
                     "from": start
                 }
             )
-
-
         ret_array = {"amount": response["hits"]["total"]["value"],
                      "pages": math.ceil(response["hits"]["total"]["value"] / length), "items": []}
         for item in response["hits"]["hits"]:
